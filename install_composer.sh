eeb5fc086edd91a8b308abf061acf7a3b65fae09
root <root@localhost.localdomain>
2022-01-25 18:14:07 +0200

    install composer and checksum

---
+++ install_composer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
